a85762093ee861142138b86beb07e5bdba1013c4
Ayush <ayushchaurasia2879@gmail.com>
2020-11-11 16:59:23 +0530

    Challenge 2: Generate Cat

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,27 @@ function ageInDays(){
 function resetAgeInDays(){
     document.getElementById('ageInDays').remove();
 }
+
+var generation = 0;
+function generateCat(){
+    var image = document.createElement("img");
+    var div = document.getElementById("flex-cat-gen");
+    image.src = "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cat_16x9.jpg?itok=1uV8V4Gl";
+    image.setAttribute('id','imgss');
+    div.appendChild(image);
+    generation++;
+}
+
+function resetImages(){
+    if(generation>0) {
+        document.getElementById("imgss").remove();
+        generation--;
+    }
+}
+
+function resetAllImages(){
+    var full = generation;
+    for(var i=0;i<full;i++){
+        resetImages();
+    }
+}
