774e8c8ea2c33679d60687fa997d25232b42e844
Ayush <ayushchaurasia2879@gmail.com>
2020-11-13 00:19:16 +0530

    Challenge 5: Blackjack game with manual bot(without automatic bot)

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+// const { func } = require("prop-types");
+
 //Challenge 1
 function ageInDays() {
     var birthYear = prompt("Type your birth year");
@@ -144,5 +146,170 @@ function randomColors() {
         // console.log(randomNumber);
         allButtons[i].classList.remove(allButtons[i].classList[1]);
         allButtons[i].classList.add(choices[randomNumber]);
+    }
+}
+
+//Challenge 5: Blackjack
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
+
+
+let blackjackGame = {
+    'you': { 'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0 },
+    'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'],
+    'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'Q': 10, 'J': 10, 'A': [1, 11] },
+    'wins': 0,
+    'losses': 0,
+    'draws': 0,
+    'isStand': false,
+    'turnsOver': false
+};
+
+
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+const hitSound = new Audio('static/sounds/swish.m4a');
+const winSound = new Audio('static/sounds/cash.mp3');
+const lostSound = new Audio('static/sounds/aww.mp3');
+
+
+function blackjackHit() {
+    if (blackjackGame['isStand'] === false) {
+        let card = randomCard();
+        showCard(YOU, card);
+        updateScore(YOU, card);
+        showScore(YOU);
+    }
+}
+
+function randomCard() {
+    let numb = Math.floor(Math.random() * 13);
+    // console.log(numb);
+    return blackjackGame['cards'][numb];
+}
+
+
+function showCard(activePlayer, card) {
+    if (activePlayer['score'] <= 21) {
+        let cardImage = document.createElement('img');
+        cardImage.src = `static/images/${card}.png`;
+        document.querySelector(activePlayer['div']).appendChild(cardImage);
+        hitSound.play();
+    }
+}
+
+function blackjackDeal() {
+    if (blackjackGame['turnsOver'] === true) {
+        blackjackGame['isStand'] = false;
+        let yourImages = document.querySelector(YOU['div']).querySelectorAll('img');
+        let dealerImages = document.querySelector(DEALER['div']).querySelectorAll('img');
+        for (let i = 0; i < yourImages.length; i++) {
+            yourImages[i].remove();
+        }
+        for (let i = 0; i < dealerImages.length; i++) {
+            dealerImages[i].remove();
+        }
+        YOU['score'] = 0;
+        DEALER['score'] = 0;
+
+        document.querySelector(YOU['scoreSpan']).textContent = 0;
+        document.querySelector(DEALER['scoreSpan']).textContent = 0;
+
+        document.querySelector(YOU['scoreSpan']).style.color = '#ffffff';
+        document.querySelector(DEALER['scoreSpan']).style.color = '#ffffff';
+
+        document.querySelector('#blackjack-result').textContent = "Let's Play";
+        document.querySelector('#blackjack-result').style.color = 'black';
+
+        blackjackGame['turnsOver'] = true;
+    }
+
+}
+
+function updateScore(activePlayer, card) {
+    if (card === 'A' && activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21)
+        activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+    else if (card === 'A')
+        activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+    else
+        activePlayer['score'] += blackjackGame['cardsMap'][card];
+}
+
+function showScore(activePlayer) {
+    if (activePlayer['score'] > 21) {
+        document.querySelector(activePlayer['scoreSpan']).textContent = "!BUST";
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    }
+    else
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+}
+
+
+function dealerLogic() {
+    blackjackGame['isStand'] = true;
+    let card = randomCard();
+    showCard(DEALER, card);
+    updateScore(DEALER, card);
+    showScore(DEALER);
+
+    if (DEALER['score'] > 15) {
+        blackjackGame['turnsOver'] = true;
+        showResult(computeWinner());
+    }
+    // showResult(computeWinner());
+}
+
+function computeWinner() {
+    let winner;
+
+    if (YOU['score'] <= 21) {
+        if (DEALER['score'] > 21 || YOU['score'] > DEALER['score']) {
+            winner = YOU;
+            blackjackGame['wins']++;
+        }
+        else if (YOU['score'] < DEALER['score']) {
+            winner = DEALER;
+            blackjackGame['losses']++;
+        }
+        else if (YOU['score'] === DEALER['score']) {
+            blackjackGame['draws']++;
+        }
+    }
+    else if (YOU['score'] > 21 && DEALER['score'] <= 21) {
+        winner = DEALER;
+        blackjackGame['losses']++;
+    }
+    else if (YOU['score'] > 21 && DEALER['score'] > 21) {
+        blackjackGame['draws']++;
+    }
+    return winner;
+}
+
+function showResult(winner) {
+    let message, messageColor;
+    if (blackjackGame['turnsOver'] === true) {
+        if (winner === YOU) {
+            document.querySelector('#wins').textContent = blackjackGame['wins'];
+            message = 'You Win!';
+            messageColor = 'green';
+            winSound.play();
+        }
+        else if (winner === DEALER) {
+            document.querySelector('#losses').textContent = blackjackGame['losses'];
+            message = 'You Lost!';
+            messageColor = 'red';
+            lostSound.play();
+        }
+        else {
+            document.querySelector('#draws').textContent = blackjackGame['draws'];
+            message = 'You Drew!';
+            messageColor = 'black';
+        }
+        document.querySelector('#blackjack-result').textContent = message;
+        document.querySelector('#blackjack-result').style.color = messageColor;
     }
 }
